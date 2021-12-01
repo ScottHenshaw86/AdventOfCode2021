c37dff7c8bd1cf175ea97161f3711d04a24ed865
Scott <scott.henshaw86@gmail.com>
2021-12-01 20:22:53 +0900

    shortened day1 a bit

---
+++ day01/script.js
@@ -0,0 +1,16 @@
+// Advent Of Code 2021 - Day 1: Part 1
+const fs = require("fs");
+
+// import my puzzle input and format it into an array of numbers
+const input = fs
+  .readFileSync("./input.txt", "latin1")
+  .split(/[(\r\n)(,)]/g)
+  .map(Number);
+
+let count = 0;
+
+input.forEach((n, i) => {
+  if (n > input[i - 1]) count++;
+});
+
+console.log(count);
