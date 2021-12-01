86ed5659f1485592de60df061f917135ce01845d
Scott <scott.henshaw86@gmail.com>
2021-12-01 20:13:03 +0900

    day 1 finished, prep for day 2

--- Day01-2021/script.js
+++ Day01-2021/script.js
@@ -1,23 +1,18 @@
 // Advent Of Code 2021 - Day 1: Part 1
-
 const fs = require("fs");
 
 // import my puzzle input and format it into an array of numbers
-// there's probably a more elegant way to do this.
-var text = fs
-  .readFileSync("./input.txt")
-  .toString()
+const input = fs
+  .readFileSync("./input.txt", "latin1")
   .split(/[(\r\n)(,)]/g)
   .map(Number);
 
 let count = 0;
-let prev = text[0];
+let prev = input[0];
 
-for (let i = 1; i < text.length; i++) {
-  if (text[i] > prev) {
-    count++;
-  }
-  prev = text[i];
-}
+input.forEach((n) => {
+  if (n > prev) count++;
+  prev = n;
+});
 
 console.log(count);
--- Day01-2021/script2.js
+++ Day01-2021/script2.js
@@ -1,27 +1,22 @@
 // Advent Of Code 2021 - Day 1: Part 2
-
 const fs = require("fs");
 
 // import my puzzle input and format it into an array of numbers
-// there's probably a more elegant way to do this.
-var text = fs
-  .readFileSync("./input.txt")
-  .toString()
+const input = fs
+  .readFileSync("./input.txt", "latin1")
   .split(/[(\r\n)(,)]/g)
   .map(Number);
 
 let count = 0;
-let a = text[0];
-let b = text[1];
-let c = text[2];
+let a = input[0];
+let b = input[1];
+let c = input[2];
 
-for (let i = 3; i < text.length; i++) {
-  if (text[i] + b + c > a + b + c) {
-    count++;
-  }
-  a = text[i - 2];
-  b = text[i - 1];
-  c = text[i];
-}
+input.forEach((n, i) => {
+  if (n + b + c > a + b + c) count++;
+  a = b;
+  b = c;
+  c = n;
+});
 
 console.log(count);
