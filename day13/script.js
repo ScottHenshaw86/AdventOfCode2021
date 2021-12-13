298930cd3c043a849327b19e0c4abe41acaf3377
Scott <scott.henshaw86@gmail.com>
2021-12-13 17:28:06 +0900

    working on day 13

--- day13/script.js
+++ day13/script.js
@@ -8,28 +8,50 @@ const dots = input.filter((a) => parseInt(a) > -1).map((b) => b.split(","));
 const folds = input.filter((a) => a.includes("fold"));
 const fold1 = folds[0];
 
-console.log(dots);
-
 const paper = [];
+
+let maxX = 10;
+let maxY = 14;
+
+for (let y = 0; y < maxY + 1; y++) {
+  paper.push([]);
+  for (let x = 0; x < maxX + 1; x++) {
+    paper[y].push(".");
+  }
+}
 
 for (let i = 0; i < dots.length; i++) {
   const x = parseInt(dots[i][0]);
   const y = parseInt(dots[i][1]);
-  if (!paper[y]) paper[y] = [];
   paper[y][x] = "#";
 }
 
-let newY = 0;
-for (let y = paper.length - 1; y > 7; y--) {
-  for (let x = 0; x < paper[0].length; x++) {
-    if (paper[y]?.[x] === "#") {
-      if (!paper[newY]) paper[newY] = [];
-      paper[newY][x] === "#";
+const foldY = () => {
+  let newY = 0;
+  for (let y = paper.length - 1; y > 7; y--) {
+    for (let x = 0; x < paper[0].length; x++) {
+      if (paper[y][x] === "#") {
+        paper[newY][x] = "#";
+      }
+    }
+    newY++;
+  }
+};
+
+const foldX = () => {
+  for (let y = 0; y < paper.length; y++) {
+    let newX = 0;
+    for (let x = paper[0].length - 1; x > 0; x--) {
+      if (paper[y][x] === "#") {
+        paper[y][newX] = "#";
+      }
+      newX++;
     }
   }
-  newY++;
-}
+};
 
-paper.splice(0, 7);
+foldX();
+
+paper.splice(7);
 
 console.log(paper);
