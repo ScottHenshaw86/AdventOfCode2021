981ea4a5fb27f8c6d5c8be30b399a6276948f70b
ScottHenshaw86 <scott.henshaw86@gmail.com>
2021-12-30 10:38:27 +0900

    guess day 19

--- day19/script.js
+++ day19/script.js
@@ -3,19 +3,12 @@ const fs = require("fs");
 
 // import my puzzle input and format it into an array of numbers
 const input = fs
-  .readFileSync("./sample.txt", "latin1")
-  .split(/--- scanner [0-9] ---\r\n/g)
-  .map((a) =>
-    a
-      .split("\n")
-      .filter((b) => b.length > 0)
-      .map((c) =>
-        c.split(",").map((d) => Math.round((parseInt(d) + 1000) / 40))
-      )
-  );
+  .readFileSync("./input.txt", "latin1")
+  .split(/--- scanner [0-9] ---\r\n/g).map(a => a.split("\r\n")).map(b => b.filter(d => d !== "")).map(c => c.map(d => d.split(",")))
+
 input.shift();
 
-// console.log(input);
+console.log(input);
 // [
 //   [ i
 //     [404, -588, -901], j[0][0], j[0][1], j[0][2]
@@ -27,24 +20,26 @@ input.shift();
 //   ],
 // ];
 
-const grid = [];
+let overlaps = 0;
 
-const c = input.length;
-for (let i = 1; i < 2; i++) {
-  let grid = [];
-
-  for (let j = 0; j < 50; j++) {
-    grid.push(Array(50).fill("."));
+for (let i=0; i<input.length; i++) { // loop through input arrays
+  for (let j=0; j<input[i].length; j++) { // loop through scanner array
+    overlaps++;
+    for (let k=0; k<input[i][j].length; k++) { // loop through scanner array
+      console.log(input[i][j][k])
+    }
   }
-  for (let j = 0; j < input[0].length; j++) {
-    let x = input[i][j][0];
-    let y = input[i][j][1];
-    // console.log(typeof x);
-    // console.log(`x: ${x} y: ${y}`);
-    grid[x][y] = "B";
-    // console.log(grid[x][y]);
-    // console.log(grid);
-  }
-  grid = grid.map((a) => a.join(""));
-  console.log(grid);
 }
+
+console.log(overlaps)
+
+
+// 585 too high
+// 465 too high
+// 429 too high
+// 417 X
+// 405 X
+// 393 X
+// 381 X
+// 369 
+// 357
