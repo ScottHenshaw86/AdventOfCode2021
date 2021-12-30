ab095e15d2ddebd87fc0d3df0dd0778e9486d930
Scott <scott.henshaw86@gmail.com>
2021-12-30 17:22:57 +0900

    finish day19 part 1. I hate this question

--- day19/script.js
+++ day19/script.js
@@ -4,14 +4,17 @@ const fs = require("fs");
 // import my puzzle input and format it into an array of numbers
 const input = fs
   .readFileSync("./input.txt", "latin1")
-  .split(/--- scanner [0-9] ---\r\n/g).map(a => a.split("\r\n")).map(b => b.filter(d => d !== "")).map(c => c.map(d => d.split(",")))
+  .split(/--- scanner [0-9] ---\n/g)
+  .map((a) => a.split("\n"))
+  .map((b) => b.filter((d) => d !== ""))
+  .map((c) => c.map((d) => d.split(",")));
 
 input.shift();
 
-console.log(input);
+// console.log(input);
 // [
-//   [ i
-//     [404, -588, -901], j[0][0], j[0][1], j[0][2]
+//   [
+//     [404, -588, -901],
 //     [528, -643, 409],
 //   ],
 //   [
@@ -19,27 +22,42 @@ console.log(input);
 //     [605, 423, 415],
 //   ],
 // ];
+let totalBeacons = input.reduce((a, b) => a + b.length, 0);
 
-let overlaps = 0;
+let diffs = [];
 
-for (let i=0; i<input.length; i++) { // loop through input arrays
-  for (let j=0; j<input[i].length; j++) { // loop through scanner array
-    overlaps++;
-    for (let k=0; k<input[i][j].length; k++) { // loop through scanner array
-      console.log(input[i][j][k])
+const c = input.length;
+for (let i = 0; i < c; i++) {
+  // loop through input arrays
+  diffs.push([]);
+  for (let j = 0; j < input[i].length; j++) {
+    // loop through beacons
+    diffs[i].push([]);
+    for (let k = 0; k < input[i].length; k++) {
+      // loop through rest of beacons
+      if (j === k) continue; // don't need distance from self
+      const x = Math.abs(input[i][j][0] - input[i][k][0]);
+      const y = Math.abs(input[i][j][1] - input[i][k][1]);
+      const z = Math.abs(input[i][j][2] - input[i][k][2]);
+      let totalDiff = x * y * z;
+      diffs[i][j].push(totalDiff);
     }
   }
 }
 
-console.log(overlaps)
+diffs = diffs.flat();
 
+const d = diffs.length;
+console.log(d);
+for (let i = 0; i < d; i++) {
+  console.log(`i: ${i}`);
+  for (let j = i + 1; j < d; j++) {
+    let overlaps = 0;
+    diffs[i].forEach((a) => {
+      if (diffs[j].indexOf(a) > -1) overlaps++;
+    });
+    if (overlaps > 10) totalBeacons--;
+  }
+}
 
-// 585 too high
-// 465 too high
-// 429 too high
-// 417 X
-// 405 X
-// 393 X
-// 381 X
-// 369 
-// 357
+console.log(totalBeacons);
